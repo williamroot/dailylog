a5451de57eb4478eb5ba4eeb2784f640e134ce45
Rodolfo Viana <rodolfo@rodolfoviana.com.br>
2018-01-10 23:12:49 -0200

    Update scraper_alesp_gastos_gabinetes.py

--- 2018/scraper_alesp_gastos_gabinetes.py
+++ 2018/scraper_alesp_gastos_gabinetes.py
@@ -5,41 +5,82 @@ Criado em 8 de janeiro de 2018, às 00:49:56
 @author: rodolfoviana
 """
 
-import urllib.request
+from __future__ import print_function
+from xml.sax import ContentHandler, parse
+import requests
 import xml.etree.cElementTree as ET
-import pandas as pd
+import csv
 
-req = urllib.request.urlopen('http://www.al.sp.gov.br/repositorioDados/deputados/despesas_gabinetes.xml')
+file = requests.get('http://www.al.sp.gov.br/repositorioDados/deputados/despesas_gabinetes.xml')
+
+with open('despesas_gabinetes.xml','wb') as f:
+	f.write(file.content)
+
+class Handler(ContentHandler):
+
+    def __init__(self):
+        ContentHandler.__init__(self)
+        self.despesa = {}
+        self.despesas = []
+        self.current_field = None
+        self.in_despesa = False
+        self.indent = 0
+
+    def startElement(self, name, attrs):
+        print('{}Start element: {}'.format('\t' * self.indent, name))
+        self.indent += 1
+        if self.in_despesa:
+            self.current_field = name
+            self.despesa[name] = ''
+
+        if name == 'despesa':
+            self.in_despesa = True
+
+    def endElement(self, name):
+        self.indent -= 1
+        print('{}End element: {}'.format('\t' * self.indent, name))
+        if name == 'despesa':
+            self.despesas.append(self.despesa)
+            self.despesa = {}
+            self.in_despesa = False
+            self.current_field = None
+
+    def characters(self, content):
+        if content.strip():
+            print('{}chars: {}'.format('\t' * self.indent, repr(content)))
+        if self.in_despesa and self.current_field:
+            self.despesa[self.current_field] += content
 
 
-def getvalueofnode(node):
-    return node.text if node is not None else None
+def gravar_despesas(writer, despesas):
+    cabecalhos = [
+        'Deputado',
+        'Matricula',
+        'Ano',
+        'Mes',
+        'Tipo',
+        'CNPJ',
+        'Fornecedor',
+        'Valor'
+    ]
+    writer.writerow(cabecalhos)
+
+    for despesa in despesas:
+        writer.writerow([despesa.get(c, '').strip() for c in cabecalhos])
 
 
-parsed_xml = ET.parse(req)
-dfcols = ['deputado', 'matricula', 'ano', 'mes', 'tipo', 'cnpj', 'fornecedor', 'valor']
+def main():
 
-df_xml = pd.DataFrame(columns=dfcols)
+    handler = Handler()
+    with open('despesas_gabinetes.xml', 'r', encoding='utf-8') as xml_file:
+        parse(xml_file, handler)
 
-for node in parsed_xml.getroot():
-    deputado = node.find('Deputado')
-    matricula = node.find('Matricula')
-    ano = node.find('Ano')
-    mes = node.find('Mes')
-    tipo = node.find('Tipo')
-    cnpj = node.find('CNPJ')
-    fornecedor = node.find('Fornecedor')
-    valor = node.find('Valor')
+    print('Found {} despesas'.format(len(handler.despesas)))
 
-    df_xml = df_xml.append(
-    	    pd.Series([getvalueofnode(deputado), 
-    		           getvalueofnode(matricula), 
-    		           getvalueofnode(ano),
-    		           getvalueofnode(mes),
-    		           getvalueofnode(tipo),
-    		           getvalueofnode(cnpj),
-    		           getvalueofnode(fornecedor),
-    		           getvalueofnode(valor)], index=dfcols),
-            ignore_index=True)
+    with open('despesas.csv', 'w', encoding='latin-1') as f_handle:
+        writer = csv.writer(f_handle)
+        gravar_despesas(writer, handler.despesas)
 
-df_xml.to_csv('gastos_gabinetes.csv', sep=';')
+
+if __name__ == "__main__":
+    main()
