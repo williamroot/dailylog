9982a5c61e733bf28f13dc358145c2a36392fbcc
Rodolfo Viana <rodolfo@rodolfoviana.com.br>
2018-04-16 22:07:34 -0300

    Add file

---
+++ scripts/belchior.py
@@ -0,0 +1,16 @@
+import random
+import time
+import webbrowser
+
+belchior_breaks = 3
+break_count = 0
+belchior_songs = ['SEsWJJAZ8pY', 'nPbTs9ZJcfU', 'fDHQZ6v3OAA',
+                  '3lwSZxYTCFU', 'js9CIyvm9rE', 'TeIiPgCNwj8']
+
+while(break_count < belchior_breaks):
+    time.sleep(7200)
+    for song in belchior_songs:
+        song = random.choice(belchior_songs)
+        url = 'https://www.youtube.com/watch?v=' + song
+        webbrowser.open(url, new = 2)
+    break_count += 1
