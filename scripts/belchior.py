1500fbc9158d6d023e15379cc48087d60dd99371
Rodolfo Viana <rodolfo@rodolfoviana.com.br>
2018-04-16 22:58:30 -0300

    Update belchior.py

--- scripts/belchior.py
+++ scripts/belchior.py
@@ -5,7 +5,8 @@ import webbrowser
 belchior_breaks = 3
 break_count = 0
 belchior_songs = ['SEsWJJAZ8pY', 'nPbTs9ZJcfU', 'fDHQZ6v3OAA',
-                  '3lwSZxYTCFU', 'js9CIyvm9rE', 'TeIiPgCNwj8']
+                  '3lwSZxYTCFU', 'js9CIyvm9rE', 'TeIiPgCNwj8',
+                  'IICn0oM52zE']
 
 while(break_count < belchior_breaks):
     time.sleep(7200)
