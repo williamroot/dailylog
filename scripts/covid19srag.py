e881ab7f6363d2b743f979e590ce2ee98bf377a3
Rodolfo Viana <rodolfo.viana@tvglobo.com.br>
2020-04-12 17:03:23 -0300

    Update deaths script; add SRAG script

---
+++ scripts/covid19srag.py
@@ -0,0 +1,76 @@
+import os
+import glob
+
+import requests
+import pandas as pd
+
+DATA_DIR = f'data'
+UF = {
+    'Acre': 'AC', 'Alagoas': 'AL', 'Amapá': 'AP',
+    'Amazonas': 'AM', 'Bahia': 'BA', 'Ceará': 'CE',
+    'Distrito Federal': 'DF', 'Espírito Santo': 'ES', 'Goiás': 'GO',
+    'Maranhão': 'MA', 'Mato Grosso': 'MT', 'Mato Grosso do Sul': 'MS',
+    'Minas Gerais': 'MG', 'Pará': 'PA', 'Paraíba': 'PB',
+    'Paraná': 'PR', 'Pernambuco': 'PE', 'Piauí': 'PI',
+    'Rio de Janeiro': 'RJ', 'Rio Grande do Norte': 'RN', 'Tocantins': 'TO',
+    'Rio Grande do Sul': 'RS', 'Rondônia': 'RO', 'Roraima': 'RR',
+    'Santa Catarina': 'SC', 'São Paulo': 'SP', 'Sergipe': 'SE'
+}
+
+
+def create_dir():
+    """
+    Cria o diretório `data`, onde serão guardados os arquivos
+    temporários e, ao fim, o arquivo `obitos.csv`. Este
+    diretório é sobrescrito cada vez que o script é rodado.
+    """
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+
+
+def fetch_srag_data():
+    """
+    Faz o download de todos os `json` de casos de SRAG do site da
+    Fiocruz. Cada semana epidemiológica gera um `json`; temos,
+    portanto, mais de 400 `json` desde 2012.
+
+    Cada `json` é convertido em `csv`. Concatenados, eles foram um
+    dataset que é limpo e tem os nomes dos estados trocados por
+    siglas.
+
+    OUTPUT: `srag_fiocruz.csv`
+    """
+    for y in range(2012, 2021):
+        for w in range(1, 54):
+            url = f'http://info.gripe.fiocruz.br/data/detailed/1/2/{y}/{w}/1/Brasil/data-table'
+            data = requests.get(url)
+            raw_response = data.json()
+            response = raw_response['data']
+            df = pd.DataFrame.from_dict(response)
+            df['ano'] = f'{y}'
+            df.to_csv(f'raw_srag_fiocruz_{y}_{w}.csv', index=False)
+
+    final = pd.concat(
+        [pd.read_csv(f) for f in glob.glob(f'raw_srag_fiocruz_*.csv')],
+        sort=False
+    )
+    final = final[['ano', 'epiweek', 'territory_name', 'value']]
+    final = final[final['territory_name'] != 'Brasil']
+    final['value'] = final['value'].str.split(r"\s", expand=True)
+    final.rename(columns={
+        'epiweek': 'semana_epidem',
+        'territory_name': 'uf',
+        'value': 'casos'
+    }, inplace=True)
+    final.replace(UF, inplace=True)
+    final['semana_epidem'] = final['semana_epidem'].astype(int)
+    final['casos'] = final['casos'].astype(int)
+    final.to_csv('srag_fiocruz.csv', index=False)
+
+
+if __name__ == '__main__':
+    create_dir()
+    os.chdir(DATA_DIR)
+    fetch_srag_data()
+    for f in glob.glob(f'raw_srag_fiocruz_*.csv'):
+        os.remove(f)  # Apaga os arquivos temporários
