c5899fbcffaaa9ed0feca2c07ce8db5c532687c6
Rodolfo Viana <rodolfo.viana@tvglobo.com.br>
2020-07-21 00:14:13 -0300

    Add output

--- studies/2020_huw_collingbourne_c_programming_for_beginners/01_basic/03_printf/main.c
+++ studies/2020_huw_collingbourne_c_programming_for_beginners/01_basic/03_printf/main.c
@@ -20,3 +20,27 @@ main() {
 	 * que serão usados na substituição.
 	 */ 
 }
+
+/* 
+ * INPUT:
+ * $ /bin/sh -f /usr/lib/codelite/codelite_exec ./03_printf
+ * 
+ * OUTPUT:
+ * This is a 'Hello world' with printf.
+ * And this is a 'Hello world' with puts.
+ * 
+ * But printf accepts format specifiers such as %s for strings and %d for numbers.
+ * Example: 20 bottles of gin.
+ * Press ENTER to continue...
+ *
+ * 
+ * BUILD INPUT:
+ * /bin/sh -c '/usr/bin/make -j8 -e -f  Makefile'
+ * 
+ * BUILD OUTPUT:
+ * ----------Building project:[ 03_printf - Debug ]----------
+ * make[1]: Entering directory '/home/rodolfo/Documents/Github/dailylog/studies/2020_huw_collingbourne_c_programming_for_beginners/01_basic/03_printf'
+ * make[1]: Nothing to be done for 'all'.
+ * make[1]: Leaving directory '/home/rodolfo/Documents/Github/dailylog/studies/2020_huw_collingbourne_c_programming_for_beginners/01_basic/03_printf'
+ * ====0 errors, 0 warnings====
+ */
